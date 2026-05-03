d4aea207fa11b34692d9394c34deaa50833ff659
Sahal Shihabudheen <sahalshihabudheen@gmail.com>
2026-05-03 08:50:24 +0530

    fix: generate missing routes and add router plugin to vite config

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -9,18 +9,43 @@
 // Additionally, you should also exclude this file from your linter and/or formatter to prevent it from being checked or modified.
 
 import { Route as rootRouteImport } from './routes/__root'
+import { Route as UploadRouteImport } from './routes/upload'
 import { Route as SearchRouteImport } from './routes/search'
-import { Route as IndexRouteImport } from './routes/index'
+import { Route as AuthRouteImport } from './routes/auth'
 import { Route as AdultRouteImport } from './routes/adult'
+import { Route as AdminRouteImport } from './routes/admin'
+import { Route as IndexRouteImport } from './routes/index'
+import { Route as MyComicIdRouteImport } from './routes/my-comic.$id'
+import { Route as MyChapterIdRouteImport } from './routes/my-chapter.$id'
 import { Route as MangaIdRouteImport } from './routes/manga.$id'
 import { Route as ChapterIdRouteImport } from './routes/chapter.$id'
 import { Route as ApiImageRouteImport } from './routes/api.image'
 import { Route as ApiMangadexSplatRouteImport } from './routes/api.mangadex.$'
 import { Route as ApiCoversSplatRouteImport } from './routes/api.covers.$'
 
+const UploadRoute = UploadRouteImport.update({
+  id: '/upload',
+  path: '/upload',
+  getParentRoute: () => rootRouteImport,
+} as any)
 const SearchRoute = SearchRouteImport.update({
   id: '/search',
   path: '/search',
+  getParentRoute: () => rootRouteImport,
+} as any)
+const AuthRoute = AuthRouteImport.update({
+  id: '/auth',
+  path: '/auth',
+  getParentRoute: () => rootRouteImport,
+} as any)
+const AdultRoute = AdultRouteImport.update({
+  id: '/adult',
+  path: '/adult',
+  getParentRoute: () => rootRouteImport,
+} as any)
+const AdminRoute = AdminRouteImport.update({
+  id: '/admin',
+  path: '/admin',
   getParentRoute: () => rootRouteImport,
 } as any)
 const IndexRoute = IndexRouteImport.update({
@@ -28,9 +53,14 @@ const IndexRoute = IndexRouteImport.update({
   path: '/',
   getParentRoute: () => rootRouteImport,
 } as any)
-const AdultRoute = AdultRouteImport.update({
-  id: '/adult',
-  path: '/adult',
+const MyComicIdRoute = MyComicIdRouteImport.update({
+  id: '/my-comic/$id',
+  path: '/my-comic/$id',
+  getParentRoute: () => rootRouteImport,
+} as any)
+const MyChapterIdRoute = MyChapterIdRouteImport.update({
+  id: '/my-chapter/$id',
+  path: '/my-chapter/$id',
   getParentRoute: () => rootRouteImport,
 } as any)
 const MangaIdRoute = MangaIdRouteImport.update({
@@ -61,32 +91,47 @@ const ApiCoversSplatRoute = ApiCoversSplatRouteImport.update({
 
 export interface FileRoutesByFullPath {
   '/': typeof IndexRoute
+  '/admin': typeof AdminRoute
   '/adult': typeof AdultRoute
+  '/auth': typeof AuthRoute
   '/search': typeof SearchRoute
+  '/upload': typeof UploadRoute
   '/api/image': typeof ApiImageRoute
   '/chapter/$id': typeof ChapterIdRoute
   '/manga/$id': typeof MangaIdRoute
+  '/my-chapter/$id': typeof MyChapterIdRoute
+  '/my-comic/$id': typeof MyComicIdRoute
   '/api/covers/$': typeof ApiCoversSplatRoute
   '/api/mangadex/$': typeof ApiMangadexSplatRoute
 }
 export interface FileRoutesByTo {
   '/': typeof IndexRoute
+  '/admin': typeof AdminRoute
   '/adult': typeof AdultRoute
+  '/auth': typeof AuthRoute
   '/search': typeof SearchRoute
+  '/upload': typeof UploadRoute
   '/api/image': typeof ApiImageRoute
   '/chapter/$id': typeof ChapterIdRoute
   '/manga/$id': typeof MangaIdRoute
+  '/my-chapter/$id': typeof MyChapterIdRoute
+  '/my-comic/$id': typeof MyComicIdRoute
   '/api/covers/$': typeof ApiCoversSplatRoute
   '/api/mangadex/$': typeof ApiMangadexSplatRoute
 }
 export interface FileRoutesById {
   __root__: typeof rootRouteImport
   '/': typeof IndexRoute
+  '/admin': typeof AdminRoute
   '/adult': typeof AdultRoute
+  '/auth': typeof AuthRoute
   '/search': typeof SearchRoute
+  '/upload': typeof UploadRoute
   '/api/image': typeof ApiImageRoute
   '/chapter/$id': typeof ChapterIdRoute
   '/manga/$id': typeof MangaIdRoute
+  '/my-chapter/$id': typeof MyChapterIdRoute
+  '/my-comic/$id': typeof MyComicIdRoute
   '/api/covers/$': typeof ApiCoversSplatRoute
   '/api/mangadex/$': typeof ApiMangadexSplatRoute
 }
@@ -94,53 +139,101 @@ export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
   fullPaths:
     | '/'
+    | '/admin'
     | '/adult'
+    | '/auth'
     | '/search'
+    | '/upload'
     | '/api/image'
     | '/chapter/$id'
     | '/manga/$id'
+    | '/my-chapter/$id'
+    | '/my-comic/$id'
     | '/api/covers/$'
     | '/api/mangadex/$'
   fileRoutesByTo: FileRoutesByTo
   to:
     | '/'
+    | '/admin'
     | '/adult'
+    | '/auth'
     | '/search'
+    | '/upload'
     | '/api/image'
     | '/chapter/$id'
     | '/manga/$id'
+    | '/my-chapter/$id'
+    | '/my-comic/$id'
     | '/api/covers/$'
     | '/api/mangadex/$'
   id:
     | '__root__'
     | '/'
+    | '/admin'
     | '/adult'
+    | '/auth'
     | '/search'
+    | '/upload'
     | '/api/image'
     | '/chapter/$id'
     | '/manga/$id'
+    | '/my-chapter/$id'
+    | '/my-comic/$id'
     | '/api/covers/$'
     | '/api/mangadex/$'
   fileRoutesById: FileRoutesById
 }
 export interface RootRouteChildren {
   IndexRoute: typeof IndexRoute
+  AdminRoute: typeof AdminRoute
   AdultRoute: typeof AdultRoute
+  AuthRoute: typeof AuthRoute
   SearchRoute: typeof SearchRoute
+  UploadRoute: typeof UploadRoute
   ApiImageRoute: typeof ApiImageRoute
   ChapterIdRoute: typeof ChapterIdRoute
   MangaIdRoute: typeof MangaIdRoute
+  MyChapterIdRoute: typeof MyChapterIdRoute
+  MyComicIdRoute: typeof MyComicIdRoute
   ApiCoversSplatRoute: typeof ApiCoversSplatRoute
   ApiMangadexSplatRoute: typeof ApiMangadexSplatRoute
 }
 
 declare module '@tanstack/react-router' {
   interface FileRoutesByPath {
+    '/upload': {
+      id: '/upload'
+      path: '/upload'
+      fullPath: '/upload'
+      preLoaderRoute: typeof UploadRouteImport
+      parentRoute: typeof rootRouteImport
+    }
     '/search': {
       id: '/search'
       path: '/search'
       fullPath: '/search'
       preLoaderRoute: typeof SearchRouteImport
+      parentRoute: typeof rootRouteImport
+    }
+    '/auth': {
+      id: '/auth'
+      path: '/auth'
+      fullPath: '/auth'
+      preLoaderRoute: typeof AuthRouteImport
+      parentRoute: typeof rootRouteImport
+    }
+    '/adult': {
+      id: '/adult'
+      path: '/adult'
+      fullPath: '/adult'
+      preLoaderRoute: typeof AdultRouteImport
+      parentRoute: typeof rootRouteImport
+    }
+    '/admin': {
+      id: '/admin'
+      path: '/admin'
+      fullPath: '/admin'
+      preLoaderRoute: typeof AdminRouteImport
       parentRoute: typeof rootRouteImport
     }
     '/': {
@@ -150,11 +243,18 @@ declare module '@tanstack/react-router' {
       preLoaderRoute: typeof IndexRouteImport
       parentRoute: typeof rootRouteImport
     }
-    '/adult': {
-      id: '/adult'
-      path: '/adult'
-      fullPath: '/adult'
-      preLoaderRoute: typeof AdultRouteImport
+    '/my-comic/$id': {
+      id: '/my-comic/$id'
+      path: '/my-comic/$id'
+      fullPath: '/my-comic/$id'
+      preLoaderRoute: typeof MyComicIdRouteImport
+      parentRoute: typeof rootRouteImport
+    }
+    '/my-chapter/$id': {
+      id: '/my-chapter/$id'
+      path: '/my-chapter/$id'
+      fullPath: '/my-chapter/$id'
+      preLoaderRoute: typeof MyChapterIdRouteImport
       parentRoute: typeof rootRouteImport
     }
     '/manga/$id': {
@@ -197,23 +297,19 @@ declare module '@tanstack/react-router' {
 
 const rootRouteChildren: RootRouteChildren = {
   IndexRoute: IndexRoute,
+  AdminRoute: AdminRoute,
   AdultRoute: AdultRoute,
+  AuthRoute: AuthRoute,
   SearchRoute: SearchRoute,
+  UploadRoute: UploadRoute,
   ApiImageRoute: ApiImageRoute,
   ChapterIdRoute: ChapterIdRoute,
   MangaIdRoute: MangaIdRoute,
+  MyChapterIdRoute: MyChapterIdRoute,
+  MyComicIdRoute: MyComicIdRoute,
   ApiCoversSplatRoute: ApiCoversSplatRoute,
   ApiMangadexSplatRoute: ApiMangadexSplatRoute,
 }
 export const routeTree = rootRouteImport
   ._addFileChildren(rootRouteChildren)
   ._addFileTypes<FileRouteTypes>()
-
-import type { getRouter } from './router.tsx'
-import type { createStart } from '@tanstack/react-start'
-declare module '@tanstack/react-start' {
-  interface Register {
-    ssr: true
-    router: Awaited<ReturnType<typeof getRouter>>
-  }
-}
